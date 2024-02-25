0d503e64dc7c0112b351ecd4090a061629cd3e8e
theabdul97@gmail.com <theabdul97@gmail.com>
2024-02-25 18:10:34 +0530

    "Previous&NextButtonFunctionality"

--- src/components/News.js
+++ src/components/News.js
@@ -13,22 +13,40 @@ export class News extends Component {
 
   async componentDidMount(){
     console.log("CDM");
-    let url= "https://newsapi.org/v2/top-headlines?country=in&apiKey=dc0b7f4c367c4b679e8e1fe8b065a4de";
+    let url= "https://newsapi.org/v2/top-headlines?country=in&apiKey=dc0b7f4c367c4b679e8e1fe8b065a4de&page=1pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
     this.setState({articles: parsedData.articles});
   }
-  handlePrevClick = () =>(
+  handlePrevClick = async ()=>{
     console.log("Previous")
-  )
-  handleNextClick = () =>(
-    console.log("Next")
-  )
+    let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=dc0b7f4c367c4b679e8e1fe8b065a4de&page=${this.state.page-1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page-1,
+      articles: parsedData.articles
+    })
+  }
+
+  handleNextClick = async () =>{
+    console.log("Next");
+
+    let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=dc0b7f4c367c4b679e8e1fe8b065a4de&page=${this.state.page+1}pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles
+    })
+}
   
 
   render() {
-    console.log("render")
     return (
       <div className="container my-3">
         <h2>NewsMonkey - Top Headlines</h2>
@@ -41,7 +59,7 @@ export class News extends Component {
         })} 
         </div>
         <div className="container d-flex justify-content-between">
-        <button type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Prev</button>
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
         <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
