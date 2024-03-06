51f62dd4b9825335000ecc4d8aa626ff160fe015
theabdul97@gmail.com <theabdul97@gmail.com>
2024-03-06 23:55:02 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
-import { BrowserRouter as Router,
+import {
+  BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from 'react-router-dom';
 
 export default class App extends Component {
-  
+
   render() {
     return (
       <div>
         <Router>
-        <Navbar/>
-        <Switch>
-          <Route path="/"><News pageSize={5} country="in" category="general"/></Route>                         </Router>
+          <Navbar/>
+          <News pageSize={5} country="in" category="general" />
+          <Switch>
+            <Route path="/about">
+              <About/>
+            </Route>
         </Switch>
-        </Router>
-      </div>
-    )
-  }
+      </Router>
+    </div>
+  )
+}
 }
