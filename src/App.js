166e13d1dd18bdb94fa604fb6bea24b36cc2adbf
theabdul97@gmail.com <theabdul97@gmail.com>
2024-03-08 23:58:10 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from 'react-router-dom';
 
 export default class App extends Component {
@@ -18,12 +17,16 @@ export default class App extends Component {
       <div>
         <Router>
           <Navbar/>
-          <News pageSize={5} country="in" category="general" />
           <Switch>
-            <Route path="/about">
-              <About/>
-            </Route>
-        </Switch>
+            <Route exact path="/"><News pageSize={5} country="in" category="general" /></Route>
+            <Route exact path="/business"><News pageSize={5} country="in" category="business" /></Route>
+            <Route exact path="/entertainment"><News pageSize={5} country="in" category="entertainment" /></Route>
+            <Route exact path="/general"><News pageSize={5} country="in" category="general" /></Route>
+            <Route exact path="/health"><News pageSize={5} country="in" category="health" /></Route>
+            <Route exact path="/science"><News pageSize={5} country="in" category="science" /></Route>
+            <Route exact path="/sports"><News pageSize={5} country="in" category="sports" /></Route>
+            <Route exact path="/technology"><News pageSize={5} country="in" category="technology" /></Route>
+          </Switch>
       </Router>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {Link} from 'react-router-dom';
 
 export class Navbar extends Component {
   static propTypes = {};
@@ -8,9 +9,9 @@ export class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">
+            <Link className="navbar-brand" to="/">
               NewsMonkeys
-            </a>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -28,29 +29,29 @@ export class Navbar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/home">
+                  <a className="nav-link active" aria-current="page" to="/home">
                     Home
                   </a>
                 </li>
                 {/* <li className="nav-item">
-                  <a className="nav-link" href="/about">
+                  <Link className="nav-link" to="/about">
                     About
-                  </a>
+                  </Link>
                 </li> */}
                 <li className="nav-item">
-                  <a className="nav-link" href="/business">Business</a></li>
+                  <Link className="nav-link" to="/business">Business</Link></li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/entertainment">Entertainment</a></li>
+                  <Link className="nav-link" to="/entertainment">Entertainment</Link></li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/general">General</a></li>
+                  <Link className="nav-link" to="/general">General</Link></li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/health">Health</a></li>
+                  <Link className="nav-link" to="/health">Health</Link></li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/science">Science</a></li>
+                  <Link className="nav-link" to="/science">Science</Link></li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/sports">Sports</a></li>
+                  <Link className="nav-link" to="/sports">Sports</Link></li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/technology">Technology</a></li>
+                  <Link className="nav-link" to="/technology">Technology</Link></li>
                 
               </ul>
             </div>
