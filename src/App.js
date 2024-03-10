4136344bac80c1dc425149a8b14b0b7976a7c61e
theabdul97@gmail.com <theabdul97@gmail.com>
2024-03-10 22:53:47 +0530

    "newsBasedOnCategory"

--- src/App.js
+++ src/App.js
@@ -18,14 +18,14 @@ export default class App extends Component {
         <Router>
           <Navbar/>
           <Switch>
-            <Route exact path="/"><News pageSize={5} country="in" category="general" /></Route>
-            <Route exact path="/business"><News pageSize={5} country="in" category="business" /></Route>
-            <Route exact path="/entertainment"><News pageSize={5} country="in" category="entertainment" /></Route>
-            <Route exact path="/general"><News pageSize={5} country="in" category="general" /></Route>
-            <Route exact path="/health"><News pageSize={5} country="in" category="health" /></Route>
-            <Route exact path="/science"><News pageSize={5} country="in" category="science" /></Route>
-            <Route exact path="/sports"><News pageSize={5} country="in" category="sports" /></Route>
-            <Route exact path="/technology"><News pageSize={5} country="in" category="technology" /></Route>
+            <Route exact path="/"><News key="general" pageSize={5} country="in" category="general" /></Route>
+            <Route exact path="/business"><News key="business"  pageSize={5} country="in" category="business" /></Route>
+            <Route exact path="/entertainment"><News key="entertainment" pageSize={5} country="in" category="entertainment" /></Route>
+            <Route exact path="/general"><News key="general" pageSize={5} country="in" category="general" /></Route>
+            <Route exact path="/health"><News key="health" pageSize={5} country="in" category="health" /></Route>
+            <Route exact path="/science"><News key="science" pageSize={5} country="in" category="science" /></Route>
+            <Route exact path="/sports"><News key="sports" pageSize={5} country="in" category="sports" /></Route>
+            <Route exact path="/technology"><News key="technology" pageSize={5} country="in" category="technology" /></Route>
           </Switch>
       </Router>
     </div>
