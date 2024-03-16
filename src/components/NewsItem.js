e0407511b8800ec27a4002fc5c1945441c091acb
theabdul97@gmail.com <theabdul97@gmail.com>
2024-03-16 17:24:16 +0530

    "seesion32"

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,24 +1,44 @@
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
-
-    constructor(){
-        super();
-        console.log("Hello I am a constructor")
-    }
+  constructor() {
+    super();
+    console.log("Hello I am a constructor");
+  }
   render() {
-    let {title, description, imageUrl, newsUrl,author, date} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className="my-3">
-        <div className="card" >
-          <img src={!imageUrl?"https://static.toiimg.com/thumb/msid-107827681,width-1070,height-580,imgsize-514350,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg": imageUrl} className="card-img-top" alt="..." />
+        <div className="card">
+          <img
+            src={
+              !imageUrl
+                ? "https://static.toiimg.com/thumb/msid-107827681,width-1070,height-580,imgsize-514350,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">
+              {title}
+              <span class="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{left:'90%', zIndex:'1'}}>
+                {source}  
+              </span>
+            </h5>
+            <p className="card-text">{description}...</p>
             <p className="card-text">
-              {description}...
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
             </p>
-            <p className="card-text"><small className="text-muted">By {!author?"Unknown" : author} on {date}</small></p>
-            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-dark"
+            >
               Read more...
             </a>
           </div>
